e67845dc7e704f8206ba67c46e5a78d16f7b6957
Oz Zapata <oz.zapata@gmail.com>
2013-02-12 00:47:34 -0500

    added new message

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -22,6 +22,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
         System.out.println("Bye Bye World!");
+        System.out.println("Hello again");
 	}
 
 }
