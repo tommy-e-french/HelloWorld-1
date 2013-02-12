09a74a30060f002af3b9e566c0c7faaeea0d4727
Oz Zapata <oz.zapata@gmail.com>
2013-02-11 23:51:28 -0500

    initial revision

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author oz
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
